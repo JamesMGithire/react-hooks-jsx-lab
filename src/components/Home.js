13b5f8372502f0c47733159031385931315097e5
James Mburu Githire <jamesmburug24@gmail.com>
2022-09-20 21:12:15 +0300

    Added id style and content on 7 and 8

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,11 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+    <div id="home">
+      <h1 style={{color:"firebrick"}}>Your Name is {name}a Web Developer from Your City {city}</h1>
+    </div>
+  );
 }
 
 export default Home;
